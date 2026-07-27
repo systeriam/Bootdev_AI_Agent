4352782a78e55e973337d093e262acf1e83df069
systeriam <landrydb@hotmail.com>
2026-07-27 02:57:29 -0500

    E: Added file path tests

---
+++ functions/get_files_info.py
@@ -0,0 +1,18 @@
+import os
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+    if valid_target_dir == False:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if directory != ".":
+        return f'Error: "{directory}" is not a directory'
+
+    if valid_target_dir == True:
+        return f'Success: "{directory}" is within the working directory'
+
+    
---
+++ test_get_files_info.py
@@ -0,0 +1,9 @@
+from functions.get_files_info import *
+
+def test():
+    print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "/bin"))
+    print(get_files_info("calculator", "../"))
+    print(get_files_info("calculator", "main.py"))
+
+test()
