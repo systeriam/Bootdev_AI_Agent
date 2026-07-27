47882e9269115d2e01e198e19ba6e622c58b6d99
systeriam <landrydb@hotmail.com>
2026-07-27 03:53:18 -0500

    F: Added file listings

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,14 +5,23 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
     # Will be True or False
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    valid = os.path.isdir(target_dir)
 
     if valid_target_dir == False:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if directory != ".":
+    if valid == False:
         return f'Error: "{directory}" is not a directory'
 
     if valid_target_dir == True:
-        return f'Success: "{directory}" is within the working directory'
-
+        contents = os.listdir(target_dir)
+        content_list = []
+        #print(f'Success: "{directory}" is within the working directory')
+        for item in contents:
+            full = os.path.join(target_dir, item)
+            try:
+                content_list.append(f"- {item}: file_size={os.path.getsize(full)} bytes, is_dir={os.path.isdir(full)}")
+            except Exception:
+                return "Error: File unsupported"
+        return "\n".join(content_list)
     
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -2,8 +2,8 @@ from functions.get_files_info import *
 
 def test():
     print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "pkg"))
     print(get_files_info("calculator", "/bin"))
     print(get_files_info("calculator", "../"))
-    print(get_files_info("calculator", "main.py"))
 
 test()
