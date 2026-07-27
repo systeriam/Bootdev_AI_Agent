ba2628b700514bac04154ee6c1d0c0d8d4365961
systeriam <landrydb@hotmail.com>
2026-07-27 16:44:31 -0500

    H: Cleaned up logic and polished get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,22 +8,17 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         valid = os.path.isdir(target_dir)
 
-        if valid_target_dir == False:
+        if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-        if valid == False:
+        if not valid:
             return f'Error: "{directory}" is not a directory'
 
-        if valid_target_dir == True:
-            contents = os.listdir(target_dir)
-            content_list = []
-            #print(f'Success: "{directory}" is within the working directory')
-            for item in contents:
-                file_path = os.path.join(target_dir, item)
-                try:
-                    content_list.append(f"- {item}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
-                except Exception:
-                    return "Error: File unsupported"
-            return "\n".join(content_list)
+        contents = os.listdir(target_dir)
+        content_list = []
+        for item in contents:
+            file_path = os.path.join(target_dir, item)
+            content_list.append(f"- {item}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+        return "\n".join(content_list)
     except Exception as e:
         return f"Error listing files: {e}"
