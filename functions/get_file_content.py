235ebe9efa0704e5f2e0f658e5c309f98d91d93b
systeriam <landrydb@hotmail.com>
2026-07-27 20:33:59 -0500

    I: Add file reader

---
+++ functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+from config import set_limit
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_path = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs    
+        valid_file = os.path.isfile(target_path)
+
+        if not valid_path:
+                return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        if not valid_file:
+                return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        
+        with open(target_path, "r") as file:
+            file_contents = file.read(set_limit)
+            if file.read(1):
+                        file_contents += f'[...File "{file_path}" truncated at {set_limit} characters]'
+            return file_contents
+        
+
+    except Exception as e:
+        return f"Error reading files: {e}"
+
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -4,6 +4,6 @@ def test():
     print(get_files_info("calculator", "."))
     print(get_files_info("calculator", "pkg"))
     print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
+    print(get_files_info("calculator", "main.py"))
 
 test()
